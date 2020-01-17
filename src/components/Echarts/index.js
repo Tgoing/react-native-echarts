efe798ba321c422fc2020cc583c1ba7fa72515c6
Tgoing <zx1174717@163.com>
2020-01-17 22:42:31 +0800

    Update index.js

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
+import { WebView } from 'react-native-webview'
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
@@ -9,9 +10,8 @@ export default class App extends Component {
     super(props);
     this.setNewOption = this.setNewOption.bind(this);
   }
-  
 
-  componentWillReceiveProps(nextProps) {
+  componentDidUpdate(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
@@ -34,7 +34,7 @@ export default class App extends Component {
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
           originWhitelist={['*']}
-          source={require('./tpl.html')}
+          source={Platform.OS === 'ios' ? require('./tpl.html') : {uri:'file:///android_asset/tpl.html'}}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
